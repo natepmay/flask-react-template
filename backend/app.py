389ef07d76e0363ca14b1fe6630fa62e5c2e4946
Nate <nathaniel.p.may@gmail.com>
2025-05-18 09:32:26 -0700

    Add boilerplate to test API

--- backend/app.py
+++ backend/app.py
@@ -3,3 +3,10 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+API_BASE = "/api/v1"
+
+
+@app.get(f"{API_BASE}/hello")
+def hello_get():
+    return {"hello": "world"}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,17 @@
+const API_URL = "http://127.0.0.1:5000/api/v1";
+
 export default function App() {
-  return <main></main>;
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    const resp = await fetch(`${API_URL}/hello`);
+    const data = await resp.json();
+    console.log(data);
+  }
+  return (
+    <main>
+      <form onSubmit={handleSubmit}>
+        <button type="submit">Submit</button>
+      </form>
+    </main>
+  );
 }
